e48c8cd225b2e112b6f1d55ac648fe3093c67167
unknown <ahmed.hadjsaad@iit.ens.tn>
2020-09-30 18:05:12 +0200

    Updates

---
+++ precache-manifest.a1a38221c122e70733f787122bf78d09.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "081bad1e50c0b6c9bfa2ba13ac63e771",
+    "url": "/hooks/index.html"
+  },
+  {
+    "revision": "b552aed06bacc68b5e17",
+    "url": "/hooks/static/css/2.ed1ec8bd.chunk.css"
+  },
+  {
+    "revision": "bdfab4668c0c0a9f7da7",
+    "url": "/hooks/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "b552aed06bacc68b5e17",
+    "url": "/hooks/static/js/2.c83c84ab.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/hooks/static/js/2.c83c84ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdfab4668c0c0a9f7da7",
+    "url": "/hooks/static/js/main.a573860c.chunk.js"
+  },
+  {
+    "revision": "97ea7731326e2517f47c",
+    "url": "/hooks/static/js/runtime-main.8599489a.js"
+  },
+  {
+    "revision": "eff22c6156702c79f056c0b0d325aa8d",
+    "url": "/hooks/static/media/cinema.eff22c61.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/hooks/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/hooks/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/hooks/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/hooks/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/hooks/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
